5e7cbc249426391f09ff661079557aed02b2daed
CarlosCastillo10 <carloscastillo090916@gmail.com>
2022-01-15 23:29:25 -0500

    :sparkles: Adding navbar

---
+++ static/assets/js/navigation.js
@@ -0,0 +1,47 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    // options
+    const breakpoints = {
+        sm: 540,
+        md: 720,
+        lg: 960,
+        xl: 1140
+    };
+    
+    if (document.querySelector('.headroom')) {
+        var headroom = new Headroom(document.querySelector("#navbar-main"), {
+            offset: 0,
+            tolerance: {
+                up: 0,
+                down: 0
+            },
+        });
+        headroom.init();
+    }
+
+    // dropdowns to show on hover when desktop
+    if (document.body.clientWidth > breakpoints.lg) {
+        var dropdownElementList = [].slice.call(document.querySelectorAll('.navbar .dropdown-toggle'))
+        dropdownElementList.map(function (dropdownToggleEl) {
+            var dropdown = new bootstrap.Dropdown(dropdownToggleEl);
+            var dropdownMenu = document.querySelector('.dropdown-menu[aria-labelledby="' + dropdownToggleEl.getAttribute('id') + '"]');
+
+            dropdownToggleEl.addEventListener('mouseover', function () {
+                dropdown.show();
+            });
+            dropdownToggleEl.addEventListener('mouseout', function () {
+                dropdown.hide();
+            });
+
+            dropdownMenu.addEventListener('mouseover', function () {
+                dropdown.show();
+            });
+
+            dropdownMenu.addEventListener('mouseout', function () {
+                dropdown.hide();
+            });
+            
+        });
+    }
+});
